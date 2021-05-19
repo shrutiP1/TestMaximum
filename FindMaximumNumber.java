dac387786c4be9580a2d8e01d1b245d4a67f6333
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-20 01:59:31 +0530

    Find Maximum Number from Integers and Test cases for it

--- FindMaximumNumber.java
+++ FindMaximumNumber.java
@@ -1,8 +1,47 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FindMaximumNumber
 {
+	public static Integer checkIntMaximum(Integer[] x)
+	{
+		Integer max=x[0];
+		for(Integer e:x)
+		{
+			if(e.compareTo(max)>0)
+			{
+				max=e;
+			}
+		}
+		return max;
+	}
+	@Test
+	public void givenIntegerMaximumAtFirst_WhenProper_returnMaximumNumberAtFirstPosition()
+	{
+		 Integer arr[]= {7,2,3};
+		 Integer max=checkIntMaximum(arr);
+		Assert.assertEquals(arr[0], max);
+	}
+	@Test
+	public void givenIntegerMaximumAtSecond_WhenProper_returnMaximumNumberAtSecondPosition()
+	{
+		 Integer arr[]= {2,6,3};
+		 Integer max=checkIntMaximum(arr);
+		Assert.assertEquals(arr[1], max);
+	} 
+	@Test
+	public void givenIntegerMaximumAtThird_WhenProper_returnMaximumNumberAtThirdPosition()
+	{
+		 Integer arr[]= {2,6,9};
+		 Integer max=checkIntMaximum(arr);
+		Assert.assertEquals(arr[2], max);
+	} 
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome");
+		Integer arr[]= {7,2,3};
+	    System.out.println("Maximum number in integer array is "+checkIntMaximum(arr));
+	    
 		
 	}	
 
